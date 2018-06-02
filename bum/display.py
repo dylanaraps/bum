e3bd299fd637ea789eb8b34f10fcfd6780a850ae
yedhink <yedhin1998@gmail.com>
2018-06-02 18:47:38 +0530

    02 Jun 18 : Create/Update new files

--- bum/display.py
+++ bum/display.py
@@ -9,7 +9,7 @@ def init(size=250):
     player = mpv.MPV(start_event_thread=False)
     player["force-window"] = "immediate"
     player["keep-open"] = "yes"
-    player["geometry"] = f"{size}x{size}"
+    player["geometry"] = f"{-0}x{-0}"
     player["autofit"] = f"{size}x{size}"
     player["title"] = "bum"
 
