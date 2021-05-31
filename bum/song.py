dylanaraps/bum

commit d286271fa4434218c87e80a9696006ffff94edab

Added flag which allows the user to override the default cover image

bum/song.py
@@ -22,7 +22,7 @@ def init(port=6600, server="localhost"):
         os._exit(1)  # pylint: disable=W0212
 
 
-def get_art(cache_dir, size, client):
+def get_art(cache_dir, size, default_cover, client):
     """Get the album art."""
     song = client.currentsong()
 
@@ -44,10 +44,11 @@ def get_art(cache_dir, size, client):
         brainz.init()
         album_art = brainz.get_cover(song, size)
 
-        if not album_art:
+        if not album_art and default_cover:
+            shutil.copy(default_cover, cache_dir / "current.jpg")
+        elif not album_art and not default_cover:
             album_art = util.default_album_art()
-
-        util.bytes_to_file(album_art, cache_dir / file_name)
-        util.bytes_to_file(album_art, cache_dir / "current.jpg")
+            util.bytes_to_file(album_art, cache_dir / file_name)
+            util.bytes_to_file(album_art, cache_dir / "current.jpg")
 
         print(f"album: Swapped art to {song['artist']}, {song['album']}.")

bum/__main__.py
@@ -46,6 +46,8 @@ def get_args():
     arg.add_argument("--no_display",
                      action="store_true",
                      help="Only download album art, don't display.")
+    arg.add_argument("--default_cover",
+                     help="Use a custom image for the cover when artwork can't be found")
 
     return arg.parse_args()
 
@@ -68,7 +70,7 @@ def main():
     client = song.init(args.port, args.server)
 
     while True:
-        song.get_art(args.cache_dir, args.size, client)
+        song.get_art(args.cache_dir, args.size, args.default_cover, client)
         if not args.no_display:
             display.launch(disp, args.cache_dir / "current.jpg")
 

commit 306d944276f8de236246a2a8d3b55a1e0c93280d

Fix regression introduced by d286271
d286271fa4434218c87e80a9696006ffff94edab introduces a regression causing
the correct album art never to be saved, even if it is successfully
downloaded.

This commit fixes said regression.

bum/song.py
@@ -52,7 +52,9 @@ def get_art(cache_dir, size, default_cover, client):
             shutil.copy(default_cover, cache_dir / "current.jpg")
         elif not album_art and not default_cover:
             album_art = util.default_album_art()
-            util.bytes_to_file(album_art, cache_dir / file_name)
+
+        if album_art:
             util.bytes_to_file(album_art, cache_dir / "current.jpg")
+            util.bytes_to_file(album_art, file_name)
 
         print(f"album: Swapped art to {song['artist']}, {song['album']}.")